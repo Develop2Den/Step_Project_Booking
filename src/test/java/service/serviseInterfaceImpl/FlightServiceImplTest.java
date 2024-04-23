6c20600f9159dd2d468fae8718a1207743bd54d4
Maryna <83983542+maryRom01@users.noreply.github.com>
2024-04-23 05:32:58 -0400

    add tests for flights (#25)
    * add methods for booking seats
    
    * add test
    
    * update flights with random booked seats
    
    * update tests for flights
    
    ---------
    
    Co-authored-by: maryRom01 <maryna_romanets@aol.com>

--- src/test/java/service/serviseInterfaceImpl/FlightServiceImplTest.java
+++ src/test/java/service/serviseInterfaceImpl/FlightServiceImplTest.java
@@ -2,19 +2,22 @@ package service.serviseInterfaceImpl;
 
 import DAO.DAOinterfaceImpl.CollectionFlightDAO;
 import DAO.DAOinterface.FlightDAO;
+import entity.Flight;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class FlightServiceImplTest {
+    FlightDAO flightDAO = new CollectionFlightDAO();
+    FlightServiceImpl flightServiceImpl = new FlightServiceImpl(flightDAO);
 
     @Test
-    public void getFlightsForSpecificDay() {
-        FlightDAO flightDAO = new CollectionFlightDAO();
-        FlightServiceImpl flightServiceImpl = new FlightServiceImpl(flightDAO);
+    public void loadDataTest() {
         flightDAO.generateRandomFlights();
         flightServiceImpl.sendData("flights.bin");
         flightServiceImpl.loadData();
@@ -32,9 +35,38 @@ public class FlightServiceImplTest {
         FlightServiceImpl flightServiceImpl = new FlightServiceImpl(flightDAO);
         flightServiceImpl.loadData();
         flightServiceImpl.displayAllFlights();
-        String flightNumber = "AGRM40734";
+        String flightNumber = "LARM30884";
         flightServiceImpl.bookSeats(flightServiceImpl.getFlightByFlightNumber(flightNumber), 3);
-        assertTrue("Seats are not booked", flightServiceImpl.getFlightByFlightNumber(flightNumber).getBookedSeats() > 0);
+        assertTrue("Seats are not booked", flightServiceImpl.getFlightByFlightNumber(flightNumber).getBookedSeats() > 2);
         System.out.println(flightServiceImpl.getSpecificFlightDetails(flightServiceImpl.getFlightByFlightNumber(flightNumber)));
+    }
+
+    @Test
+    public void searchFlightTest() {
+        flightServiceImpl.loadData();
+        List<Flight> flights = flightServiceImpl.getAllFlightsForSpecificDay();
+        System.out.println(flights.size());
+        Date currentDate = new Date();
+        flights.forEach(flight -> {
+            assertTrue("The date should be current day or the next day",
+                    (flight.getDate().getDate() == currentDate.getDate()
+                            || flight.getDate().getDate() == currentDate.getDate() + 1
+                    )
+                            && (
+                            flight.getDate().getYear() == currentDate.getYear() + 1900
+                    )
+                            && (
+                            flight.getDate().getMonth() == currentDate.getMonth() + 1
+                    )
+            );
+        });
+    }
+
+    @Test
+    public void getFlightByFlightNumberTest() {
+        flightServiceImpl.loadData();
+        List<Flight> flights = flightServiceImpl.getAllFlightsForSpecificDay();
+        String flightNumber = flights.get(0).getFlightNumber();
+        assertEquals("Search by flight number does not return expected flight", flightNumber, flightServiceImpl.getFlightByFlightNumber(flightNumber).getFlightNumber());
     }
 }
