0182694b34bf0bd78ce189af2513b7fc2697fc60
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2024-04-24 11:50:45 +0300

    added readme

--- src/main/java/PackageMenu/ConsoleClass.java
+++ src/main/java/PackageMenu/ConsoleClass.java
@@ -12,12 +12,11 @@ import entity.Passenger;
 import entity.enums.City;
 import java.io.IOException;
 import java.text.ParseException;
-
-import java.text.ParseException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import static utils.DateConverter.stringToDate;
+import utils.Logger;
 
 public class ConsoleClass {
     private final FlightController flightController;
@@ -43,10 +42,12 @@ public class ConsoleClass {
 
     public String setName() {
         System.out.println("Введіть ваше ім'я: ");
+        Logger.info("Зберігає ім'я пассажира");
         return scanner.nextLine().trim();
     }
     public String setSurname() {
         System.out.println("Введіть ваше прізвище: ");
+        Logger.info("Зберігає прізвище пассажира");
         return scanner.nextLine().trim();
     }
     public String setDestination() {
@@ -57,8 +58,10 @@ public class ConsoleClass {
                     .map(City::name)
                     .anyMatch(cityName -> cityName.equalsIgnoreCase(input));
             if (isValidCity) {
+                Logger.info("Перевіряє та передає пункт призначення");
                 return input;
             } else {
+                Logger.error("Невірно введене місто");
                 System.out.println("Введене місто не відповідає списку доступних міст. Спробуйте ще раз.");
             }
         }
@@ -67,15 +70,16 @@ public class ConsoleClass {
         try {
             System.out.println("Введіть номер рейсу: ");
             String flightId = scanner.nextLine();
+            Logger.info("Перевіряє та передає номер рейсу");
             System.out.println(flightController.getSpecificFlightDetails(flightController.getFlightByFlightNumber(flightId)));
         } catch (FlightException e) {
+            Logger.error("Невірно введено номер рейсу");
             System.out.println("\u001B[31m" + "Помилка: " + "\u001B[0m" + e.getMessage());
         }
     }
     public void setReservationId() {
         int count = 0;
         boolean isValid = false;
-
         do {
             System.out.println("Введіть ID бронювання: ");
             String reservationId = scanner.nextLine();
@@ -84,26 +88,37 @@ public class ConsoleClass {
                 if (count < 0) {
                     System.out.println("\u001B[31m" + "Помилка: введіть додатне ціле число." + "\u001B[0m");
                 } else {
+                    Logger.info("Перевіряє та передає айді рейсу");
                     isValid = true;
                 }
             } catch (NumberFormatException e) {
+                Logger.error("Невірно введено айді рейсу");
                 System.out.println("\u001B[31m" + "Помилка: введіть ціле число." + "\u001B[0m");
             }
         } while (!isValid);
-            Flight flight = bookingController.getFlightByBookingId(count);
-            List <Integer> list =  bookingController.getBookingsIdsByPassenger(bookingController.getBookingById(count).getPassenger());
-            System.out.println(list.toString());
-            if (flight != null) {
-                try {
-                    flightController.bookSeats(flight, -1);
-                    bookingController.cancelBooking(count);
-                } catch (IOException e) {
-                    System.out.println("\u001B[31m" + "Сталася помилка при спробі скасувати бронювання." + "\u001B[0m");
+
+        Booking booking = bookingController.getBookingById(count);
+        if (booking != null) {
+            List<Integer> list = bookingController.getBookingsIdsByPassenger(booking.getPassenger());
+            if (list != null && list.contains(count)) {
+                Flight flight = bookingController.getFlightByBookingId(count);
+                if (flight != null) {
+                    try {
+                        flightController.bookSeats(flight, -1);
+                        bookingController.cancelBooking(count);
+                    } catch (IOException e) {
+                        System.out.println("\u001B[31m" + "Сталася помилка при спробі скасувати бронювання." + "\u001B[0m");
+                    }
+                } else {
+                    System.out.println("\u001B[31m" + "Помилка: рейс для бронювання з ID " + count + " не знайдено." + "\u001B[0m");
                 }
             } else {
+                System.out.println("\u001B[31m" + "Помилка: бронювання з ID " + count + " не знайдено." + "\u001B[0m");
+            }
+        } else {
             System.out.println("\u001B[31m" + "Помилка: бронювання з ID " + count + " не знайдено." + "\u001B[0m");
         }
-    }
+}
     public int setCountPassengers() {
         int count = 0;
         boolean isValid = false;
@@ -113,14 +128,17 @@ public class ConsoleClass {
             try {
                 count = Integer.parseInt(passengerCount);
                 if (count < 0) {
+                    Logger.error("Невірно введено число");
                     System.out.println("\u001B[31m" + "Помилка: введіть додатне ціле число." + "\u001B[0m");
                 } else {
                     isValid = true;
                 }
             } catch (NumberFormatException e) {
+                Logger.error("Невірно введено число");
                 System.out.println("\u001B[31m" + "Помилка: введіть ціле число." + "\u001B[0m");
             }
         } while (!isValid);
+        Logger.info("Перевіряє та передає кількість пассажирів");
         return count;
     }
     public Date setDate() {
@@ -133,9 +151,11 @@ public class ConsoleClass {
                 date = stringToDate(inputDateStr);
                 isValid = true;
             } catch (ParseException e) {
+                Logger.error("Невірно введено дату");
                 System.out.println("Невірний формат дати! Повторіть введення.");
             }
         }
+        Logger.info("Перевіряє та передає дату рейсу");
         return date;
     }
     public void flightDetails() throws InvalidMenuItemException {
@@ -144,6 +164,7 @@ public class ConsoleClass {
         int countPassengers = setCountPassengers();
         SearchFlightDTO2 infoFlight = new SearchFlightDTO2(dest, date, countPassengers);
         List<Flight> flightList = flightController.searchFlight(infoFlight);
+        Logger.info("Виводить запрошенні рейси та здійснює бронювання за бажанням пассажира");
         if (flightList.isEmpty()) {
             System.out.println("Немає відповідних рейсів!");
             return;
@@ -155,6 +176,7 @@ public class ConsoleClass {
         do {
             System.out.print("Виберіть рейс (або натисніть 0 для виходу): ");
             while (!scanner.hasNextInt()) {
+                Logger.error("Невірно введено число");
                 System.out.println( "\u001B[31m" + "Будь ласка, введіть число: " + "\u001B[0m");
                 scanner.next();
             }
@@ -176,15 +198,18 @@ public class ConsoleClass {
         try {
             flightController.bookSeats(selectedFlight,countPassengers);
         } catch (IOException e) {
+            Logger.error("Невірно введено рейс або кількість пассажирів");
             throw new RuntimeException(e);
         }
         System.out.println("Бронювання здійснено!");
     }
     public void fullName() {
+        Logger.info("виводить заброньовані рейси пассажира");
         bookingController.displayAllBookingByPassenger(new Passenger(getPassenger().getName(), getPassenger().getSurname()));
     }
     public void onlineScoreboard() {
         System.out.println("Список рейсів: ");
+        Logger.info("Виводить усі рейси");
         flightController.displayAllFlights();
         System.out.println("==========================================================================================");
     }
@@ -193,7 +218,7 @@ public class ConsoleClass {
         String userSurname = setSurname();
         System.out.print("Введіть пароль: ");
         String password = scanner.nextLine().trim();
-
+        Logger.info("Робить аунтифікацію пассажира");
         setPassenger(bookingController.getActivePassenger(userName, userSurname, password));
         return getPassenger() != null;
     }
